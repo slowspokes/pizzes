5b7688f9b55c93d16db9d585806570573556edc8
slowspokes <slowspokes089@gmail.com>
2025-02-10 15:10:29 +0000

    Commit 4 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -1,2 +1,32 @@
-Has demanat només la pizza bàsica. El preu és 9.68 € (IVA inclòs)<br>
-autor: Ralph Bolland Riba
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>FES LA TEVA PIZZA</title>
+</head>
+<body>
+<?php
+$preu = 8;
+$IVA = 21;
+
+if (empty($_GET['ingr'])) {
+    $pvp = ((100 + $IVA) / 100) * $preu;
+    echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+} else {
+    $IVA = 21;
+    $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
+    echo "Has demanat la pizza bàsica amb:<br>";
+
+    foreach ($_GET['ingr'] as $ingredient) {
+        if ($ingredient) {
+            echo "* $ingredient<br>";
+        }
+    }
+
+    echo "El preu és $pvp € (IVA inclòs)<br>";
+}
+
+echo "<br>";
+echo "Autor: rabori" . "<br>";
+?>
+</body>
+</html>
